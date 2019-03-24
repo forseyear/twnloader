d4769d54fa17107e358af5de749d12d1f25da0e2
forseyear <forseyear@users.noreply.github.com>
2019-03-24 12:15:36 +0900

    first commit

---
+++ config_exsample.js
@@ -0,0 +1,7 @@
+module.exports = {
+  Consumer_Key: 'API_Key',
+  Consumer_Secret: 'Consumer_Secret',
+  Access_Token: 'Access_Token',
+  Access_Secret: 'Access_Secret',
+  File_Name: 'url.txt'
+};
---
+++ main.js
@@ -0,0 +1,9 @@
+const Twitter = require('twitter');
+const Config = require('./config.js');
+const axios = require('axios');
+const fs = require('fs');
+
+(async () => {
+
+
+})();
